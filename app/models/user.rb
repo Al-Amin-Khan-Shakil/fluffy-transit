7471013c257ffb9dc65994b4d06574e76926f958
Al-Amin <alaminkhanshakil@gmail.com>
2024-03-12 21:56:10 +0600

    Create service model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  has_many :services
+
   ROLES = %w[admin user].freeze
 
   validates :role, inclusion: { in: ROLES }
