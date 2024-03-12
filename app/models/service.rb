1532ba056c1d3bd059accf3e5808afbf3cd110dc
Al-Amin <alaminkhanshakil@gmail.com>
2024-03-12 22:07:04 +0600

    Add image attachement and validate params on sevice

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,8 @@
 class Service < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :image
+
+  validates :name, :description, :image, presence: true
+  validates :min_cost, presence: true, numericality: { greater_than: 0 } 
 end
