00deafe9edaf9c47558920d30c199323bcb7396d
Al-Amin <alaminkhanshakil@gmail.com>
2024-03-12 23:42:37 +0600

    Create new service through api

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::ServicesController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+
   def index
     @services = Service.all
     render json: Api::V1::ServicesRepresenter.new(@services).as_json
+  end
+
+  def create
+    @service = current_user.services.build(service_params)
+
+    if @service.save
+      render json: { data: 'Service was added successfully', status: 'success' }, status: :created
+    else
+      render json: { errors: @service.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
